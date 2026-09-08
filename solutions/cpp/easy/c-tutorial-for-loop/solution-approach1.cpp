3873c42ea5cf775a766f02c15738649befcb9940
Parthak Singh <152584992+parthaksingh@users.noreply.github.com>
2026-09-09 01:07:49 +0530

    ✅ For Loop [cpp] — approach1

---
+++ solutions/cpp/easy/c-tutorial-for-loop/solution-approach1.cpp
@@ -0,0 +1,49 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem?isFullScreen=true
+// Problem     For Loop
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-09-09, 01:07 a.m.
+// ──────────────────────────────────────────────────
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin>> a>>b;
+    
+    for(int i = a; i <= b; i++){
+        if(i == 1)
+            cout << "one" << endl;
+        else if(i == 2)
+            cout << "two" << endl;
+        else if(i == 3)
+            cout << "three" << endl;
+        else if(i == 4)
+            cout << "four" << endl;
+        else if(i == 5)
+            cout << "five" << endl;
+        else if(i == 6)
+            cout << "six" << endl;
+        else if(i == 7)
+            cout << "seven" << endl;
+        else if(i == 8)
+            cout << "eight" << endl;
+        else if(i == 9)
+            cout << "nine" << endl;
+        else {
+            if(i % 2 == 0)
+                cout << "even" << endl;
+            else
+                cout << "odd" << endl;
+        }
+        }
+    
+    // Complete the code.
+    return 0;
+}
